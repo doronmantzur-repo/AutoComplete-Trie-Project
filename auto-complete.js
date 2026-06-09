a6422a91be0ad96e2779f47c42b5369cc9ece522
doronm <doronm@chain-reaction.io>
2026-06-09 22:23:04 +0300

    refactor addWord, findWord, and predictWords methods to include root parameter

--- auto-complete.js
+++ auto-complete.js
@@ -4,17 +4,32 @@ class trieNode {
     this.value = value;
     this.endOfWord = false;
   }
-  addWord(word) {
+  addWord(root, word) {
+    const characters = word.split("");
+    let currentNode = root;
+    for (let i = 0; i < characters.length; i++) {
+      const char = characters[i];
+      if (!currentNode.children[char]) {
+        currentNode.children[char] = new trieNode(char);
+      }
+      currentNode = currentNode.children[char];
+      if(i=== characters.length - 1) {
+        currentNode.endOfWord = true;
+      }
+    }
+  }
+
+  findWord(root, word) {
     const characters = word.split("");
   }
 
-  findWord(word) {
-    const characters = word.split("");
-  }
-
-  predictWords(prefix) {
+  predictWords(root, prefix) {
     const characters = prefix.split("");
   }
 }
 
 const root = new trieNode("");
+root.addWord(root, "hello");
+root.addWord(root, "hi");
+root.addWord(root, "hey");
+root.addWord(root, "he");
