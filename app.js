d8c8fd2ce139609994750eee272bccb636571871
doronm <doronm@chain-reaction.io>
2026-06-10 14:07:41 +0300

    Add app file - User Interface

---
+++ app.js
@@ -0,0 +1,31 @@
+import promptSync from "prompt-sync";
+import { validate } from "revalidator";
+const prompt = promptSync();
+
+console.log("=== AutoComplete Trie Console ===");
+console.log("Type 'help' for commands");
+
+
+while (true) {
+  let selectedOption = prompt();
+  selectedOption = selectedOption.split(" ");
+  const command = selectedOption[0];
+  let argument = "";
+  
+  if (command === "exit") {
+    console.log("Exiting...");
+    break;
+  }
+  else if (command !== "help") {
+    argument = selectedOption[1];
+  }
+
+  cmdValid = validateCommands(command, argument);
+  if (!cmdValid.valid) {
+    console.log("Invalid command: " + cmdValid.errors[0].message);
+    continue;
+  }
+  
+  console.log(handleCommand(command, argument));
+
+}
